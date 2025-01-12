c57d6a96833712bf81f268a2f4cf1b3c162d8a5c
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2025-01-12 09:32:33 +0100

    added

---
+++ Animated Onboarding/Animated Onboarding/OnboardingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingViewController.swift
+//  Animated Onboarding
+//
+//  Created by Mac on 02/12/2024.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
